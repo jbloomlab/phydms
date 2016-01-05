72634ceec9fd3a8daf6373ef2d1311edd573ba09
jbloom <jbloom@fhcrc.org>
2016-01-05 06:44:35 -0800

    Docs for version 1.1.0

--- phydmslib/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-``phydmslib`` is the Python API for ``phydms``.
-"""
-from _metadata import __version__
-from _metadata import __author__
-from _metadata import __author_email__
-from _metadata import __url__
-from _metadata import __acknowledgments__
