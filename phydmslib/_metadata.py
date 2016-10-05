c7d8636f61623078832bd3f5942549db58b204d7
jbloom <jbloom@fredhutch.org>
2016-10-05 12:35:48 -0700

    Incremented version to 1.3.0

--- phydmslib/_metadata.py
+++ phydmslib/_metadata.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.dev0'
+__version__ = '1.3.0'
 __author__ = 'the Bloom lab (see https://github.com/jbloomlab/phydms/contributors for all contributors)'
 __url__ = 'http://jbloomlab.github.io/phydms'
 __author_email__ = 'jbloom@fredhutch.org'
