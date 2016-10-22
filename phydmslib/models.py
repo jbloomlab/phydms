72c1376d9010c3d367e715b11e3c9445800d541b
jbloom <jbloom@fredhutch.org>
2016-10-21 20:53:55 -0700

    Bug fix in error message

--- phydmslib/models.py
+++ phydmslib/models.py
@@ -260,7 +260,7 @@ class ExpCM(Model):
             if not (isinstance(value, scipy.ndarray) and value.shape == shape[param]
                     and value.dtype == 'float'):
                 raise ValueError(("{0} must be ndarray floats, shape {1}, "
-                        "not {2}").format(param, shape, value))
+                        "not {2}").format(param, shape[param], value))
             if not ((lowlim <= value).all() and (value <= highlim).all()):
                 raise ValueError("{0} must be >= {1} and <= {2}, not {3}".format(
                         param, lowlim, highlim, value))
