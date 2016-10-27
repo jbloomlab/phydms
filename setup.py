6008248389290836ded196c6470ca3787b01db1f
jbloom <jbloom@fredhutch.org>
2016-10-27 12:42:49 -0700

    Removed unused copy of `treelikelihood.py`

--- setup.py
+++ setup.py
@@ -16,7 +16,9 @@ except ImportError:
 
 if not ((sys.version_info[0] == 2 and sys.version_info[1] == 7) or
         (sys.version_info[0] == 3 and sys.version_info[1] >= 4)):
-    raise RuntimeError('phydms is currently only tested with Python 2.7, or Python 3.4 or higher.\nYou are using Python %d.%d' % (sys.version_info[0], sys.version_info[1]))
+    raise RuntimeError('phydms is only tested with Python 2.7, or Python 3.4 '
+            'or higher.\nYou are using Python {0}.{1}'.format(
+            sys.version_info[0], sys.version_info[1]))
 
 # get metadata, which is specified in another file
 metadata = {}
@@ -35,6 +37,31 @@ for dataname in ['version', 'author', 'author_email', 'url']:
 with open('README.rst') as f:
     readme = f.read()
 
+class lazy_cythonize(list):
+    """Lazy evaluation of cythonize so it isn't needed until installed.
+    
+    Following this:
+    http://stackoverflow.com/questions/11010151/distributing-a-shared-library-and-some-c-code-with-a-cython-extension-module
+    """
+    def __init__(self, callback):
+        self._list = None
+        self.callback = callback
+    def c_list(self):
+        if self._list is None: 
+            self._list = self.callback()
+        return self._list
+    def __iter__(self):
+        for e in self.c_list(): yield e
+    def __getitem__(self, ii): 
+        return self.c_list()[ii]
+    def __len__(self): 
+        return len(self.c_list())
+
+def extensions():
+    """List of `cython` extensions for `lazy_cythonize`."""
+    from Cython.Build import cythonize
+    return cythonize('phydmslib/*.pyx')
+
 # main setup command
 setup(
     name = 'phydms', 
@@ -42,8 +69,9 @@ setup(
     author = metadata['author'],
     author_email = metadata['author_email'],
     url = metadata['url'],
-    download_url = 'https://github.com/jbloomlab/phydms/tarball/%s' % metadata['version'], # assumes appropriate tagged version is on GitHub
-    description = 'Phylogenetic analyses informed by deep mutational scanning data.',
+    download_url = 'https://github.com/jbloomlab/phydms/tarball/{0}'.format(
+            metadata['version']), # assumes tagged version is on GitHub
+    description = 'Phylogenetic analyses informed by deep mutational scanning.',
     long_description = readme,
     license = 'GPLv3',
     install_requires = [
@@ -55,9 +83,11 @@ setup(
         'sympy>=1.0', 
         'six>=1.10',
         'pandas>=0.18',
+        'cython>=0.21',
         ],
     packages = ['phydmslib'],
     package_dir = {'phydmslib':'phydmslib'},
+    ext_modules = lazy_cythonize(extensions),
     scripts = [
             'scripts/phydms',
 #            'scripts/phydms_comprehensive',
