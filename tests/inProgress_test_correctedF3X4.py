06eba3ef4e54d66d4538841fbc0ef671399b772e
Sarah Hilton <skhilton@uw.edu>
2017-02-01 18:46:09 -0800

    Updated the calculation of phi_pw and added this function to the YNGKP_M0 model

--- tests/inProgress_test_correctedF3X4.py
+++ tests/inProgress_test_correctedF3X4.py
@@ -13,29 +13,41 @@ class testYNGKP_M0_correctedF3X4(unittest.TestCase):
         self.e_pw = self.e_pw/self.e_pw.sum(axis=1, keepdims=True)
 
         def F(phi_pw):
-            #the index of phi_x for pos `p` and nt `w` is (4*p + w) becaue of the flattend array
-            functionList = []
-            pi_x = 3 * phi_pw[0 * 4 + 3] + 2 * phi_pw[1 * 4 + 0] + phi_pw[1 * 4 + 2] + 2 * phi_pw[2 * 4 + 0] + phi_pw[2 * 4 + 2] #part of the denom
-            for p in range(3): #for each position
-                for w in range(4): #for each nucleotide, #this matches the flatten/reshape order
-                    s = 0
-                    for x in range(N_STOP): #for each stop codon
-                        if(NT_CODON_POS_TO_STOP[p][w][x]): #if `pw` part of stop codon x
-                            #STOP_CODON_INDEX_TO_NT_INDICES[x][y] gives the nucleotide at position y in codon stop
-                            s += scipy.prod(scipy.asarray([phi_pw[y * 4 + STOP_CODON_INDEX_TO_NT_INDICES[x][y]] for y in range(3)])) #multiply the phi values for given stop codon and add to s
-                    functionList.append(((phi_pw[p * 4 + w] - s / (1 - pi_x)) - self.e_pw[p][w])) #do the final calculations
-            return(scipy.asarray(functionList))
+            phi_pw_reshape = phi_pw.reshape((3,4)) #reshape flattened array
+            functionList = [] #the list of 12 equations. returned at the end
+            stop_frequency = [] #frequency of each of the three stop codons based on the phi_pw values
 
-        print(self.e_pw)
+            for x in range(N_STOP):
+                codonFrequency = STOP_CODON_TO_NT_INDICES[x] * phi_pw_reshape
+                codonFrequency = scipy.prod(codonFrequency.sum(axis=1)) #sum across each position (only one value per row will be non-zero)
+                stop_frequency.append(codonFrequency) #multiply the phi_pw frequency for each nt in codon
+            C = scipy.sum(stop_frequency) # this is the denominator from the Pond paper
+
+            for p in range(3):
+                for w in range(N_NT):
+                    s = 0
+                    for x in range(N_STOP):
+                        if STOP_CODON_TO_NT_INDICES[x][p][w] == 1: #if the nucleotide, positition pair is in the codon
+                            s += stop_frequency[x] #add the frequency of the codon
+                    functionList.append((phi_pw_reshape[p][w] - s)/(1 - C) - self.e_pw[p][w]) #final calc
+            return functionList
+
         phi_pw = self.e_pw.copy().flatten()
-        assert((phi_pw.reshape((3,4)) == self.e_pw).all())
+        assert((phi_pw.reshape((3,N_NT)) == self.e_pw).all())
         with scipy.errstate(invalid='ignore'):
             result = scipy.optimize.root(F, phi_pw,
                     tol=1e-8)
             assert result.success, "Failed: {0}".format(result)
-            phi_pw = result.x.reshape((3,4))
-        print()
-        print(phi_pw)
+            phi_pw = result.x.reshape((3,N_NT))
+            self.assertTrue(scipy.allclose(phi_pw.sum(axis = 1),
+                scipy.ones(3, dtype='float'),atol=1e-4, rtol=5e-3))
+            assert(phi_pw[0][3] >= self.e_pw[0][3]) #The calculated frequency for T at position 0 should be higher than the empirical frequency
+            assert(phi_pw[0][0] <= self.e_pw[0][0]) #The calculated frequency for A at position 0 should be lower than the numerical frequency
+
+            newValues = F(phi_pw.flatten())
+            self.assertTrue(scipy.allclose(scipy.asarray(newValues).reshape((3,4)),
+                scipy.zeros((3, N_NT), dtype='float'), atol=1e-4, rtol=5e-3))
+
 
 
 
