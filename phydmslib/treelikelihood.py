Fixed bug in branch scaling for YNGKP_M0
Previously, the `phydms` script did not add
the branch scaling parameter `mu` for `YNGKP_M0`.

phydmslib/treelikelihood.py
@@ -281,7 +281,10 @@ class TreeLikelihood(object):
         assert len(self.paramsarray) > 0, "There are no parameters to optimize"
         result = scipy.optimize.minimize(func, self.paramsarray,
                 method='L-BFGS-B', jac=dfunc, bounds=self.paramsarraybounds)
-        assert result.success, "Optimization failed:\n{0}".format(result.message) 
+        assert result.success, ("Optimization failed with this message:\n{0}\n"
+                "Current mu = {1}\nCurrent model params: {2}").format(
+                result.message, self.model.mu, ', '.join(['{0} = {1}'.format(
+                p, pvalue) for (p, pvalue) in self.model.paramsReport.items()])) 
 
         return result
 

tests/test_branchScale.py
@@ -109,7 +109,7 @@ class test_branchScale_ExpCM(unittest.TestCase):
         treedist /= float(nreplicates)
 
         # We expect nsubs = branchScale * t, but build in some tolerance
-        # of rtol = 0.1 since we simulated finite number of sites.
+        # with rtol since we simulated finite number of sites.
         self.assertTrue(scipy.allclose(nsubs, model.branchScale * t, rtol=0.1),
                 ("Simulated subs per site of {0} is not close "
                 "to expected value of {1} (branchScale = {2}, t = {3})").format(