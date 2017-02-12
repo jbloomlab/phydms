Fixed underflow for `TreeLikelihoodDistribution`
A reason that `TreeLikelihoodDistribution` was
causing problems for `YNGKP_M5` and `ExpCM` with
`--gammarates` was problems with the underflow
correction. That is now tested and fixed.

phydmslib/treelikelihood.py
@@ -473,20 +473,8 @@ class TreeLikelihood(object):
                 Mleft = self.model.M(tleft)
                 MLleft = broadcastMatrixVectorMultiply(Mleft, self.L[nlefti])
             scipy.copyto(self.L[ni], MLright * MLleft)
-            if ni > 0 and ni % self.underflowfreq == 0:
-                scale = scipy.amax(self.L[ni], axis=1)
-                self.L[ni] /= scale[:, scipy.newaxis]
-                self.underflowlogscale += scipy.log(scale)
             for param in self.model.freeparams:
-                paramvalue = getattr(self.model, param)
-                if isinstance(paramvalue, float):
-                    indices = [()] # no sub-indexing needed
-                elif (isinstance(paramvalue, scipy.ndarray) and 
-                        paramvalue.ndim == 1 and paramvalue.shape[0] > 1):
-                    # need to sub-index calculations for each param element
-                    indices = [(j,) for j in range(len(paramvalue))]
-                else:
-                    raise RuntimeError("invalid param: {0}".format(param))
+                indices = self._sub_index_param(param)
                 if istipr:
                     dMright = self.model.dM(tright, param, Mright, 
                             self.tips[nright], self.gaps[nright])
@@ -516,8 +504,29 @@ class TreeLikelihood(object):
                                 self.dL[param][nlefti][j])
                     scipy.copyto(self.dL[param][ni][j], (dMLright + MdLright)
                             * MLleft + MLright * (dMLleft + MdLleft))
-                    if ni > 0 and ni % self.underflowfreq == 0:
+            if ni > 0 and ni % self.underflowfreq == 0:
+                scale = scipy.amax(self.L[ni], axis=1)
+                self.L[ni] /= scale[:, scipy.newaxis]
+                self.underflowlogscale += scipy.log(scale)
+                for param in self.model.freeparams:
+                    indices = self._sub_index_param(param)
+                    for j in indices:
                         self.dL[param][ni][j] /= scale[:, scipy.newaxis] 
+
+    def _sub_index_param(self, param):
+        """Returns list of sub-indexes for `param`.
+
+        Used in computing partial likelihoods; loop over these indices."""
+        paramvalue = getattr(self.model, param)
+        if isinstance(paramvalue, float):
+            indices = [()] # no sub-indexing needed
+        elif (isinstance(paramvalue, scipy.ndarray) and 
+                paramvalue.ndim == 1 and paramvalue.shape[0] > 1):
+            indices = [(j,) for j in range(len(paramvalue))]
+        else:
+            raise RuntimeError("Invalid param: {0}, {1}".format(
+                    param, paramvalue))
+        return indices
 
 
 class TreeLikelihoodDistribution(TreeLikelihood):
@@ -673,25 +682,11 @@ class TreeLikelihoodDistribution(TreeLikelihood):
                     MLleft = broadcastMatrixVectorMultiply(Mleft, 
                             self.L[nlefti][k])
                 scipy.copyto(self.L[ni][k], MLright * MLleft)
-                for param in (self.model.freeparams + 
-                        [self.model.distributedparam]):
-                    if param in self.model.distributionparams:
-                        continue
-                    if param == self.model.distributedparam:
-                        indices = [()] # no sub-indexing needed
-                    else:
-                        paramvalue = getattr(self.model, param)
-                        if isinstance(paramvalue, float):
-                            indices = [()] # no sub-indexing needed
-                        elif (isinstance(paramvalue, scipy.ndarray) and 
-                                paramvalue.ndim == 1 and 
-                                paramvalue.shape[0] > 1):
-                            # sub-index for each param element
-                            indices = [(j,) for j in range(len(
-                                    paramvalue))]
-                        else:
-                            raise RuntimeError("invalid: {0}".format(
-                                    param))
+                paramlist = [param for param in self.model.freeparams +
+                        [self.model.distributedparam] if param not in
+                        self.model.distributionparams]
+                for param in paramlist:
+                    indices = self._sub_index_param(param)
                     if istipr:
                         dMright = self.model.dM(k, tright, param, Mright,
                                 self.tips[nright], self.gaps[nright])
@@ -730,7 +725,18 @@ class TreeLikelihoodDistribution(TreeLikelihood):
                 self.underflowlogscale += scipy.log(scale)
                 for k in range(self.ncats):
                     self.L[ni][k] /= scale[:, scipy.newaxis]
-                    self.dL[param][ni][k][j] /= scale[:, scipy.newaxis] 
+                    for param in paramlist:
+                        for j in indices:
+                            self.dL[param][ni][k][j] /= scale[:, scipy.newaxis] 
+
+    def _sub_index_param(self, param):
+        """See docs for `TreeLikelihood` base class."""
+        if param == self.model.distributedparam:
+            indices = [()] # no sub-indexing needed
+        else:
+            indices = super(TreeLikelihoodDistribution, self)._sub_index_param(
+                    param)
+        return indices
 
 
 if __name__ == '__main__':

tests/test_underflow.py
@@ -1,4 +1,4 @@
-"""Tests correction for numerical underflow in `TreeLikelihood`.
+"""Tests correction for numerical underflow likelihood calculations.
 
 Written by Jesse Bloom.
 """
@@ -17,7 +17,7 @@ import Bio.Phylo
 
 
 class test_underflow(unittest.TestCase):
-    """Tests correction for numerical underflow."""
+    """Test underflow correction in `TreeLikelihood` and `TreeLikelihoodDistribution`."""
 
     def test_underflow(self):
         """Tests correction for numerical underflow."""
@@ -38,19 +38,26 @@ class test_underflow(unittest.TestCase):
         prefslist = [prefs[r] for r in sites]
 
         model = phydmslib.models.ExpCM(prefslist)
+        distmodel = phydmslib.models.GammaDistributedOmegaModel(
+                model, ncats=4)
 
-        tl = phydmslib.treelikelihood.TreeLikelihood(tree, alignment, model,
-                underflowfreq=5)
-        tl_nocorrection = phydmslib.treelikelihood.TreeLikelihood(tree, 
-                alignment, model, underflowfreq=10000)
+        for (m, desc) in [(model, 'simple'), (distmodel, 'distribution')]:
+            if isinstance(m, phydmslib.models.DistributionModel):
+                tl_class = phydmslib.treelikelihood.TreeLikelihoodDistribution
+            else:
+                tl_class = phydmslib.treelikelihood.TreeLikelihood
+            tl = tl_class(tree, alignment, m, underflowfreq=1)
+            tl_nocorrection = tl_class(tree, alignment, m, underflowfreq=10000)
 
-        self.assertTrue(scipy.allclose(tl.loglik, tl_nocorrection.loglik),
-                ("Log likelihoods differ with and without correction: "
-                "{0} versus {1}".format(tl.loglik, tl_nocorrection.loglik)))
-        for (param, dl) in tl_nocorrection.dloglik.items():
-            self.assertTrue(scipy.allclose(dl, tl.dloglik[param]),
-                    ('dloglik differs with and without correction for {0}: '
-                    '{1} versus {2}'.format(param, tl.dloglik[param], dl)))
+            self.assertTrue(scipy.allclose(tl.loglik, tl_nocorrection.loglik),
+                    ("Log likelihoods differ with and without correction "
+                    "for {0}: {1} versus {2}".format(desc, tl.loglik, 
+                    tl_nocorrection.loglik)))
+            for (param, dl) in tl_nocorrection.dloglik.items():
+                self.assertTrue(scipy.allclose(dl, tl.dloglik[param]),
+                        ('dloglik differs with and without correction for {0}: '
+                        '{1}, {2} versus {3}'.format(param, desc, 
+                        tl.dloglik[param], dl)))
 
 
 if __name__ == '__main__':