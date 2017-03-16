f8f4f4cce18e2d15f1b07c4a128e044e22aa5509
Sarah Hilton <skhilton@uw.edu>
2017-03-16 10:37:45 -0700

    Fixed bug in phydms_divpressure and add expected results to test

--- tests/test_phydms_divpressure.py
+++ tests/test_phydms_divpressure.py
@@ -10,6 +10,8 @@ import os
 import shutil
 import unittest
 import subprocess
+import pandas as pd
+import scipy
 
 
 class test_phydms_testdivpressure(unittest.TestCase):
@@ -25,13 +27,24 @@ class test_phydms_testdivpressure(unittest.TestCase):
         tree = '{0}/NP_tree.newick'.format(npdir)
         for f in [prefs, alignment]:
             self.assertTrue(os.path.isfile(f), "Can't find file {0}".format(f))
-        outprefix = './divpressure_tests/results/'
+        outprefix = './divpressure_test_results/'
         if os.path.isdir(outprefix):
             shutil.rmtree(outprefix)
 
 
-        subprocess.check_call(['phydms_testdivpressure', outprefix, alignment,
+        subprocess.check_call(['phydms_divpressure', outprefix, alignment,
                 prefs, "--tree", tree, divpressure, divpressure2])
+
+        actual = pd.read_csv(outprefix + "modelcomparison.csv")
+        expected = pd.read_csv("./expected_divpressure_test_results/" \
+                + "modelcomparison.csv")
+        assert((actual.columns.values == expected.columns.values).all())
+        columns = [x for x in actual.columns.values if x != "value"]
+        actual.sort_values(by=columns, inplace=True)
+        expected.sort_values(by=columns, inplace=True)
+        self.assertTrue(scipy.allclose(actual["value"],
+                expected["value"], atol=1e-2, rtol=1e-5))
+
 
 
 if __name__ == '__main__':
