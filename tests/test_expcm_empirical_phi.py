c01df50a4d78e7f91dc2d032f02a6c14e3c81817
Sarah Hilton <skhilton@uw.edu>
2018-06-08 11:31:48 -0700

    shorted expcm empirical phi test

--- tests/test_expcm_empirical_phi.py
+++ tests/test_expcm_empirical_phi.py
@@ -21,12 +21,12 @@ class testExpCM_empirical_phi(unittest.TestCase):
         # create preferences
         random.seed(1)
         scipy.random.seed(1)
-        self.nsites = 10
+        self.nsites = 7
         self.prefs = []
         minpref = 0.01
         for r in range(self.nsites):
             rprefs = scipy.random.dirichlet([0.5] * N_AA)
-            rprefs[rprefs < minpref] = minpref 
+            rprefs[rprefs < minpref] = minpref
             rprefs /= rprefs.sum()
             self.prefs.append(dict(zip(sorted(AA_TO_INDEX.keys()), rprefs)))
 
@@ -35,7 +35,7 @@ class testExpCM_empirical_phi(unittest.TestCase):
         omega = 0.7
         kappa = 2.5
         beta = 1.2
-        self.expcm = phydmslib.models.ExpCM_empirical_phi(self.prefs, 
+        self.expcm = phydmslib.models.ExpCM_empirical_phi(self.prefs,
                 g=g, omega=omega, kappa=kappa, beta=beta)
         self.assertTrue(scipy.allclose(g, self.expcm.g))
 
@@ -65,7 +65,7 @@ class testExpCM_empirical_phi(unittest.TestCase):
         self.assertTrue(scipy.allclose(sum(nt_freqs), 3 * self.nsites))
         nt_freqs = scipy.array(nt_freqs)
         nt_freqs /= nt_freqs.sum()
-        self.assertTrue(scipy.allclose(nt_freqs, self.expcm.g, atol=1e-5), 
+        self.assertTrue(scipy.allclose(nt_freqs, self.expcm.g, atol=1e-5),
                 "Actual nt_freqs: {0}\nExpected (g): {1}".format(
                 nt_freqs, self.expcm.g))
 
@@ -78,9 +78,9 @@ class testExpCM_empirical_phi(unittest.TestCase):
             return expcm.dphi_dbeta[w]
 
         for w in range(N_NT):
-            diff = scipy.optimize.check_grad(func_phi, func_dphi, 
+            diff = scipy.optimize.check_grad(func_phi, func_dphi,
                     [self.expcm.beta], self.expcm, w, epsilon=1e-4)
-            self.assertTrue(diff < 1e-4, 
+            self.assertTrue(diff < 1e-4,
                     "dphi_dbeta diff {0} for w = {1}".format(diff, w))
         self.expcm.updateParams(self.params) # back to original value
 
@@ -97,9 +97,9 @@ class testExpCM_empirical_phi(unittest.TestCase):
 
         for r in range(self.nsites):
             for x in range(N_CODON):
-                diff = scipy.optimize.check_grad(func_prx, func_dprx, 
+                diff = scipy.optimize.check_grad(func_prx, func_dprx,
                         [self.expcm.beta], self.expcm, r, x, epsilon=1e-4)
-                self.assertTrue(diff < 1e-4, 
+                self.assertTrue(diff < 1e-4,
                         "dprx_dbeta diff {0} for r = {1}, x = {2}".format(
                         diff, r, x))
         self.expcm.updateParams(self.params) # back to original value
@@ -117,9 +117,9 @@ class testExpCM_empirical_phi(unittest.TestCase):
 
         for x in random.sample(range(N_CODON), 3):
             for y in range(N_CODON):
-                diff = scipy.optimize.check_grad(func_Qxy, func_dQxy, 
+                diff = scipy.optimize.check_grad(func_Qxy, func_dQxy,
                         [self.expcm.beta], self.expcm, x, y, epsilon=1e-4)
-                self.assertTrue(diff < 1e-4, 
+                self.assertTrue(diff < 1e-4,
                         "dQxy_dbeta diff {0} for x = {1}, y = {2}".format(
                         diff, x, y))
         self.expcm.updateParams(self.params) # back to original value
@@ -133,7 +133,7 @@ class testExpCM_empirical_phi(unittest.TestCase):
         self.assertFalse(scipy.isinf(self.expcm.Prxy).any())
         diag = scipy.eye(N_CODON, dtype='bool')
         for r in range(self.nsites):
-            self.assertTrue(scipy.allclose(0, scipy.sum(self.expcm.Prxy[r], 
+            self.assertTrue(scipy.allclose(0, scipy.sum(self.expcm.Prxy[r],
                     axis=1)))
             self.assertTrue(scipy.allclose(0, self.expcm.Prxy[r].sum()))
             self.assertTrue((self.expcm.Prxy[r][diag] <= 0).all())
@@ -159,7 +159,7 @@ class testExpCM_empirical_phi(unittest.TestCase):
         """Makes sure derivatives are as expected."""
         # check derivatives of Prxy calculated by dPrxy
         # implementation looks a bit complex because `check_grad` function
-        # can only be used for single values at a time, so have to loop 
+        # can only be used for single values at a time, so have to loop
         # over r, x, y and so hash values to make faster
 
         def funcPrxy(paramvalue, paramname, expcm, r, x, y):
@@ -184,7 +184,7 @@ class testExpCM_empirical_phi(unittest.TestCase):
             for r in random.sample(range(self.nsites), 2): # check a few sites
                 for x in random.sample(range(N_CODON), 3): # check a few codons
                     for y in range(N_CODON):
-                        diff = scipy.optimize.check_grad(funcPrxy, funcdPrxy, 
+                        diff = scipy.optimize.check_grad(funcPrxy, funcdPrxy,
                                 pvalue, pname, self.expcm, r, x, y, epsilon=1e-4)
                         self.assertTrue(diff < 1e-3, ("diff {0} for {1}:" +
                                 " r = {2}, x = {3}, y = {4}, beta = {5} " +
@@ -192,13 +192,13 @@ class testExpCM_empirical_phi(unittest.TestCase):
                                 "omega = {9}, Frxy = {10}, Prxy = {11}, " +
                                 "phi = {12}, kappa = {13}, dQxy_dbeta = {14}, " +
                                 "dphi_dbeta = {15}, dPrxy_dbeta = {16}"
-                                ).format(diff, pname, r, x, y, 
-                                self.params['beta'], self.expcm.pi_codon[r][x], 
+                                ).format(diff, pname, r, x, y,
+                                self.params['beta'], self.expcm.pi_codon[r][x],
                                 self.expcm.pi_codon[r][y], self.expcm.mu,
                                 self.expcm.omega, self.expcm.Frxy[r][x][y],
-                                self.expcm.Prxy[r][x][y], self.expcm.phi, 
+                                self.expcm.Prxy[r][x][y], self.expcm.phi,
                                 self.expcm.kappa, self.expcm.dQxy_dbeta[x][y],
-                                self.expcm.dphi_dbeta, 
+                                self.expcm.dphi_dbeta,
                                 self.expcm.dPrxy['beta'][r][x][y]))
             self.expcm.updateParams(self.params) # back to original value
 
@@ -218,7 +218,7 @@ class testExpCM_empirical_phi(unittest.TestCase):
                         "Max diff {0}".format((self.expcm.M(t)[r] - direct).max()))
         # check derivatives of M calculated by dM
         # implementation looks a bit complex because `check_grad` function
-        # can only be used for single values at a time, so have to loop 
+        # can only be used for single values at a time, so have to loop
         # over r, x, y and so hash values to make faster
         def funcM(paramvalue, paramname, t, expcm, r, x, y, storedvalues):
             """Gets `M(t)[r][x][y]`."""
@@ -258,15 +258,15 @@ class testExpCM_empirical_phi(unittest.TestCase):
                                 continue
                             diff = scipy.optimize.check_grad(funcM, funcdM, pvalue,
                                     pname, t, self.expcm, r, x, y, storedvalues,
-                                    epsilon=1e-4) 
+                                    epsilon=1e-4)
                             self.assertTrue(diff < 1e-3, ("diff {0} for {1}:" +
                                 " computed derivative = {10}, " +
                                 " r = {2}, x = {3}, y = {4}, beta = {5}, " +
                                 "pirAx = {6}, pirAy = {7}, t = {8}, mu = {9}"
-                                ).format(diff, pname, r, x, y, 
-                                self.params['beta'], self.expcm.pi_codon[r][x], 
+                                ).format(diff, pname, r, x, y,
+                                self.params['beta'], self.expcm.pi_codon[r][x],
                                 self.expcm.pi_codon[r][y], t, self.expcm.mu,
-                                funcdM(pvalue, pname, t, self.expcm, 
+                                funcdM(pvalue, pname, t, self.expcm,
                                 r, x, y, {})))
                 self.expcm.updateParams(self.params) # back to original value
 
