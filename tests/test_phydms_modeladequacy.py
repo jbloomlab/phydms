bed0509782e0c2ce7f2c469b141895d31bbc5a7f
Sarah Hilton <skhilton@uw.edu>
2018-12-14 10:51:01 -0800

    removed call to raxml and made file paths more robust

--- tests/test_phydms_modeladequacy.py
+++ tests/test_phydms_modeladequacy.py
@@ -28,17 +28,25 @@ import subprocess
 class test_modeladequacy_ExpCM_seed0(unittest.TestCase):
     """Runs model adequacy on an ExpCM."""
     # run parameters
-    MODEL = "ExpCM_modeladequacy_tests/HA_short_prefs_nsites10.csv"
-    EXPECTED = "expected_modeladequacy_results/ExpCM_pvalues_seed0.csv"
+    MODEL = "ExpCM_{0}".format(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               "modeladequacy_tests/HA_short_prefs_nsites10.csv"))
+    EXPECTED = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "expected_modeladequacy_results/ExpCM_pvalues_seed0.csv")
+    TREE = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                        "modeladequacy_tests/HA_short_nsites10_nseqs34_tree.newick")
     SEED = 0
 
     def test_modeladequacy(self):
         """Runs model adequacy and compares against expected results."""
         n_sim = 100
-        alignment = "modeladequacy_tests/HA_short_nsites10_nseqs34.fasta"
-        outprefix = "_model_adequacy_results"
+        alignment = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                 "modeladequacy_tests/HA_short_nsites10_nseqs34.fasta")
+        outprefix = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                 "_model_adequacy_results")
+        tree = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "modeladequacy_tests/HA_short_nsites10_nseqs34_tree.newick")
         cmd = ["phydms_modeladequacy", outprefix, alignment,
-               self.MODEL, "--number_simulations", str(n_sim), "--raxml", "raxml",
+               self.MODEL, "--number_simulations", str(n_sim), "--tree", tree,
                "--seed", str(self.SEED)]
         subprocess.check_call(cmd)
 
@@ -58,7 +66,8 @@ class test_modeladequacy_ExpCM_seed0(unittest.TestCase):
 class test_modeladequacy_ExpCM_seed1(test_modeladequacy_ExpCM_seed0):
     """Runs model adequacy on an ExpCM with a seed of 1."""
     # run parameters
-    EXPECTED = "expected_modeladequacy_results/ExpCM_pvalues_seed1.csv"
+    EXPECTED = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "expected_modeladequacy_results/ExpCM_pvalues_seed1.csv")
     SEED = 1
 
 
@@ -66,15 +75,16 @@ class test_modeladequacy_YNGKPM0_seed0(test_modeladequacy_ExpCM_seed0):
     """Runs model adequacy on a YNGKP_M0 with seed of 0."""
     # run parameters
     MODEL = "YNGKP_M0"
-    EXPECTED = "expected_modeladequacy_results/YNGKP_M0_pvalues_seed0.csv"
+    EXPECTED = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "expected_modeladequacy_results/YNGKP_M0_pvalues_seed0.csv")
 
 
 class test_modeladequacy_YNGKPM0_seed1(test_modeladequacy_YNGKPM0_seed0):
     """Runs model adequacy on a YNGKP_M0 with seed of 1."""
     # run parameters
     SEED = 1
-    EXPECTED = "expected_modeladequacy_results/YNGKP_M0_pvalues_seed1.csv"
-
+    EXPECTED = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "expected_modeladequacy_results/YNGKP_M0_pvalues_seed1.csv")
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
